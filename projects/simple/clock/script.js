811d0293d5412b7ba0fb15ebe1b8de12f0d865eb
rajahwu <vmradford@gmail.com>
2023-01-07 19:37:22 -0500

    add time standard change to button

--- projects/simple/clock/script.js
+++ projects/simple/clock/script.js
@@ -1,5 +1,11 @@
 const timeElement = document.getElementById('time');
+const hourDiv = document.getElementById('hour');
+const minuetDiv = document.getElementById('minuet');
+const secondDiv = document.getElementById('second');
+const am_pmDiv = document.getElementById('meridian');
+
 const dateElement = document.getElementById('date');
+const timeFormatBtn = document.getElementById('time-format');
 
 const currentDate = new Date();
 tickClock();
@@ -12,10 +18,28 @@ const currentDay = currentDate.toLocaleDateString('en-us', {
 
 dateElement.innerText = currentDay;
 
+let timeFormat = true;
 function tickClock() {
     setInterval(() => {
         const date = new Date();
-        const time = date.toLocaleTimeString();
-        timeElement.innerText = time;
-    }, 1000)
+        const hour = date.getHours();
+        const minuet = date.getMinutes();
+        const second = date.getSeconds();
+
+        if(timeFormat && hour > 12) {
+            hourDiv.innerText = hour - 12;
+        } else {
+            hourDiv.innerText = hour.toString().padStart(2, '0');
+        }
+        minuetDiv.innerText = minuet;
+        secondDiv.innerText = second.toString().padStart(2, '0');
+        am_pmDiv.innerText = hour < 12 ? 'AM' : 'PM';
+    
+    }, 10)
 }
+
+timeFormatBtn.addEventListener('click', () => {
+    timeFormat ? timeFormatBtn.innerText = 'Set to Standard Time' :
+        timeFormatBtn.innerText = 'Set to Military Time';
+    timeFormat ? timeFormat = false : timeFormat = true;
+})
